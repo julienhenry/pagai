8dce56940382d7b37495d1751dbc57bda74f24ec
Julien Henry <Julien.Henry@imag.fr>
2011-02-25 15:46:40 +0100

    New class added : Abstract

--- src/Abstract.h
+++ src/Abstract.h
@@ -1,6 +1,8 @@
 #ifndef _ABSTRACT_H
 #define _ABSTRACT_H
 
+#include <vector>
+
 #include "ap_global1.h"
 
 class Node;
@@ -17,10 +19,32 @@ class Abstract {
 
 		Abstract(ap_manager_t* _man);
 
+		Abstract(Abstract* A);
+
 		~Abstract();
+
+		void set_top(ap_environment_t * env);
+
+		void set_bottom(ap_environment_t * env);
+
+		void change_environment(ap_environment_t * env);
 
 		bool is_leq(Abstract * d);
 
 		void widening(Node * n);
+
+		void meet_tcons_array(ap_tcons1_array_t* tcons);
+
+		void canonicalize();
+
+		void assign_texpr_array(
+				ap_var_t* tvar, 
+				ap_texpr1_t* texpr, 
+				size_t size, 
+				ap_abstract1_t* dest);
+		
+		void join_array(ap_environment_t * env, std::vector<Abstract*> X_pred);
+
+		void print();
 };
 #endif
