efb0e5c72356e4bf094cb6187944f4c251d043c0
Julien Henry <Julien.Henry@imag.fr>
2011-09-26 10:37:04 +0200

    rate limiter example

--- ex/rate_limiter.c
+++ ex/rate_limiter.c
@@ -5,11 +5,11 @@ void rate_limiter() {
   x_old = 0;
   while (1) {
     int x = input();
-    if (x <= -1000 || x >= 1000) x=0;
-    if (x >= x_old+1)
-        x = x_old+1;
-    if (x <= x_old-1)
-        x = x_old-1;
+    if (x < -10000 || x > 10000) x=0;
+    if (x >= x_old+10)
+        x = x_old+10;
+    if (x <= x_old-10)
+        x = x_old-10;
     x_old = x;
   }
 }
