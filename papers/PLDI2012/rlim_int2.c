6b80ddf09b3db756f88edb7c449dafdf90582044
David Monniaux <david.monniaux@imag.fr>
2011-10-21 07:56:44 +0200

    input(a, b) mieux que input() suivi de tests pour exclure out-of-range

--- papers/PLDI2012/rlim_int2.c
+++ papers/PLDI2012/rlim_int2.c
@@ -2,8 +2,7 @@ extern int input();
 void rate_limiter() {
   int x_old = 0;
   while (1) {
-    int x = input();
-    if (x < -100000 || x > 100000) x=0;
+    int x = input(-100000, 100000);
     if (x > x_old+10) x = x_old+10;
     else if (x < x_old-10) x = x_old-10;
     else x_old = x;
--- papers/PLDI2012/rlim_int_loop.c
+++ papers/PLDI2012/rlim_int_loop.c
@@ -3,8 +3,7 @@ extern int wait();
 void rate_limiter() {
   int x_old = 0;
   while (1) {
-    int x = input();
-    if (x < -100000 || x > 100000) x=0;
+    int x = input(-100000, 100000);
     if (x > x_old+10) x = x_old+10;
     if (x < x_old-10) x = x_old-10;
     x_old = x;
