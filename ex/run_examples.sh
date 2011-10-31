606e336a75315e51fee66bc2e7672fdac800fb42
Julien Henry <Julien.Henry@imag.fr>
2011-10-31 11:10:55 +0100

    run_example.sh

--- ex/run_examples.sh
+++ ex/run_examples.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
-OLD_DIR=`pwd`
-cd /home/jhenry/code/ex
-
 for FILENAME in `ls *.{c,cpp}` ; do
 	echo "Running $FILENAME..."
-	analyzer.sh -i $FILENAME -u  > results/$FILENAME.result
+	./analyzer.sh -i $FILENAME -u  > results/$FILENAME.result
 done
-
-
-
-cd $OLD_DIR
