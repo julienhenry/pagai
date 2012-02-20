6c5c384eeecfd249decdff13c2040cb4ca07ed4f
Julien Henry <Julien.Henry@imag.fr>
2012-02-20 15:11:44 +0100

    assume

--- ex/rate_limiter.c
+++ ex/rate_limiter.c
@@ -1,11 +1,14 @@
 
 extern int input();
+extern void assume(int predicate);
+
 void rate_limiter() {
   int x_old;
   x_old = 0;
   while (1) {
     int x = input();
-    if (x < -100000 || x > 100000) x=0;
+	assume (x >= -100000);
+	assume (x <= 100000);
     if (x > x_old+10)
         x = x_old+10;
     if (x < x_old-10)
