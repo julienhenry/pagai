ed09fded7e40206bd336ba191538d32bb7770a7e
Julien Henry <Julien.Henry@imag.fr>
2013-12-16 23:29:35 +0100

    optbr

--- opt/optbr.cc
+++ opt/optbr.cc
@@ -9,27 +9,28 @@
 using namespace llvm;
 
 void Optbr::visitBranchInst(BranchInst &I) {
+	bool isok = true;
 	if (I.isConditional()) {
 		BasicBlock * succ = I.getSuccessor(0);
-		for (unsigned i = 1; i < I.getNumSuccessors(); i++) {
-			if (succ != I.getSuccessor(i))
+		if (succ != I.getSuccessor(1))
 				return;
-		}
 		// conditionnal branch that always goes to the same successor
 		// one can transform this branch into an unconditional one
 		BasicBlock * parent = I.getParent();
-		I.eraseFromParent();
 		//BranchInst * newbr = BranchInst::Create(succ,parent);
+		LLVMContext &ctx = parent->getContext();
+		BasicBlock * newblock = BasicBlock::Create(ctx,"avoid.same.destination",parent->getParent(),succ);
+		BranchInst::Create(succ,newblock);
+		I.setSuccessor(1,newblock);
+		llvm::outs() << *newblock << "\n";
 
 		for (BasicBlock::iterator i = succ->begin(), e = succ->end(); i != e; ++i) {
 			if (PHINode * phi = dyn_cast<PHINode>(i)) {
-restart:
 				unsigned seen = 0;
 				for (unsigned i = 0; i < phi->getNumIncomingValues(); i++) {
 					if (phi->getIncomingBlock(i) == parent) {
 						if (seen > 0) {
-							phi->removeIncomingValue(i);
-							goto restart;
+							phi->setIncomingBlock(i,newblock);
 						}
 						seen++;
 					}
