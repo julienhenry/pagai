a1e16e3803c2581ba2afa52b89d92d4c0dffec97
David Monniaux <David.Monniaux@imag.fr>
2014-02-11 19:00:26 +0100

    qelim

--- arrays/sentinel4.py
+++ arrays/sentinel4.py
@@ -1,4 +1,4 @@
-from z3 import Reals, Ints, Tactic, Exists, ForAll, Or, And, Implies, Then
+from z3 import Reals, Ints, Tactic, Exists, ForAll, Or, And, Implies, Then, Not
 i,x,n = Ints('i x n')
 # i,x,n = Reals('i x n') h2 does not imply h
 
@@ -15,8 +15,11 @@ f = Or(
   And(-i+x==0, -i+n-3>=0, i>=0))
 g = And(n>2, ForAll(x, Implies(And(x>=0, x<n), f)))
 h = apply(And,tactic(g)[0])
+
 h2 = And(n>2, i>=0, i<=n-2)
+#h2neg = Or(n<=2, i<=-1, i>=n-1)
 
 print tactic(ForAll(x,Implies(h,h2)))
+print tactic(ForAll(x,Implies(h2,h)))
 
-print tactic(ForAll(x,Implies(h,h2)))
+#print tactic(ForAll(x,Or(h2neg,h)))
