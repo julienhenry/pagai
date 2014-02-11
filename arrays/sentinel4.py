d4d17e998060a0824a8a5f0202bac75c0c15fcfb
David Monniaux <David.Monniaux@imag.fr>
2014-02-11 17:09:49 +0100

    other tactic

--- arrays/sentinel4.py
+++ arrays/sentinel4.py
@@ -2,7 +2,7 @@ from z3 import Ints, Tactic, Exists, ForAll, Or, And, Implies, Then
 i,x,n = Ints('i x n')
 
 #tactic = Then(Tactic('qe'),Tactic('simplify'))
-tactic = Tactic('qe')
+tactic = Tactic('qe-light')
 f = Or(
   And(-i+x==0, -i+n-1==0, i-2>=0),
   And(-n+x+2==0, -i+n-3>=0, i>=0),
