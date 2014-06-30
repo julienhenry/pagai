8943d0eab285684ebc29050ec8190b53b11baf83
Julien Henry <Julien.Henry@imag.fr>
2014-06-30 11:27:25 +0200

    scripts

--- scripts/build_table_svcomp.py
+++ scripts/build_table_svcomp.py
@@ -3,6 +3,7 @@
 import sys
 import json
 import os
+import re
 
 input = sys.argv[1]
 if os.path.isfile(input):
@@ -27,23 +28,38 @@ def escape_latex(name):
     }
     escaped_name="".join([CHARS.get(char, char) for char in name])
     if len(escaped_name) > 15:
-        index = escaped_name.find('\_true')
+        #index = escaped_name.find('\_true')
+        escaped_name = escaped_name.replace('\_true-unreach-label','')
+        escaped_name = escaped_name.replace('\_cilled','')
+        escaped_name = escaped_name.replace('32\_1\_ok\_nondet\_linux-3.4-32\_1-drivers--','')
+        escaped_name = escaped_name.replace('32\_7a\_linux-3.8-rc1-32\_7a-drivers--','')
+        escaped_name = escaped_name.replace('32\_7a\_linux-3.8-rc1-32\_7a-','')
+        escaped_name = escaped_name.replace('32\_7a\_linux-3.8-rc1-drivers--','')
+
         #index = 25
+        index = escaped_name.find('\_sequence')
         escaped_name = escaped_name[0:index]
     return escaped_name
 
 
 def begin_tabular():
-    print r"\begin{tabular}{|l|r|S|r|S|}\hline"
+    print r"\begin{longtable}{|l|r|S|r|S|}\hline"
 def end_tabular():
     print r"\hline"
-    print r"\end{tabular}"
+    print r"\end{longtable}"
 
 def print_line_header():
     print \
     "Benchmark & result & time & result & time"\
     + r"\\ \hline"
 
+def remove_color(str):
+    ansi_escape = re.compile(r'\x1b[^m]*m')
+    return ansi_escape.sub('', str)
+
+def print_result(str):
+    return str.replace('UNKNOWN (ERROR)','error')
+    return str
 
 begin_tabular()
 print_line_header()
@@ -59,9 +75,9 @@ for benchmark_name in json_dict:
     if "FALSE" in json_dict[benchmark_name]["box"]["expected"]:
         continue
     print escape_latex(benchmark_name) \
-    + ' & ' + str(json_dict[benchmark_name]["pk"]["result"])\
-    + ' & ' + str(json_dict[benchmark_name]["pk"]["time"])\
-    + ' & ' + str(json_dict[benchmark_name]["box"]["result"])\
-    + ' & ' + str(json_dict[benchmark_name]["box"]["time"])\
+    + ' & ' + print_result(str(json_dict[benchmark_name]["pk"]["result"]))\
+    + ' & ' + remove_color(str(json_dict[benchmark_name]["pk"]["time"]))\
+    + ' & ' + print_result(str(json_dict[benchmark_name]["box"]["result"]))\
+    + ' & ' + remove_color(str(json_dict[benchmark_name]["box"]["time"]))\
     + r"\\"
 end_tabular()
